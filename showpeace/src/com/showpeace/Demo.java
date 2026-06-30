b4f9129330cd086a0d391ffd20a5af9a2d0b9f51
Manikanta-9704 <manikanta.a9704@gmail.com>
2026-06-30 23:00:28 +0530

    uygkj

---
+++ showpeace/src/com/showpeace/Demo.java
@@ -0,0 +1,13 @@
+package com.showpeace;
+
+public class Demo {
+	Demo(String name){
+		System.out.println(name);
+	}
+
+	public static void main(String[] args) {
+		
+Demo d1=new Demo("suresh");
+	}
+
+}
