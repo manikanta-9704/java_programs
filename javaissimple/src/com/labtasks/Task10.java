a52638089eb0adb5e1a14e30f15838ef3da80ea1
Manikanta-9704 <manikanta.a9704@gmail.com>
2026-07-03 10:36:33 +0530

    task 10 added

---
+++ javaissimple/src/com/labtasks/Task10.java
@@ -0,0 +1,16 @@
+package com.labtasks;
+
+public class Task10 {
+	void calculateArea(){
+		float length=20;
+		float breadth=30;
+			System.out.println(length*breadth);
+		
+	}
+	public static void main(String[] args) {
+		//Write a Java program to create a class "Rectangle" with a method "calculateArea()". The method should take no arguments and return no value. Initialize the length and breadth inside the method, calculate the area of the rectangle, and display the result.
+Task10 t1=new Task10();
+t1.calculateArea();
+	}
+
+}
