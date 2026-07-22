dba30f27d7b61833ad4308edae5702b8f3176418
Manikanta-9704 <manikanta.a9704@gmail.com>
2026-07-22 17:08:51 +0530

    reverse array

---
+++ javaissimple/src/com/practice/ReverseArray.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+	int[] numbers= {43,23,45,66,54};
+    
+	for (int i=numbers.length-1;i>=0;i--){
+		
+		
+		System.out.print(numbers[i]+" ");
+		
+	}
+
+	}
+
+}
